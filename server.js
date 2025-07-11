2ee310f252713195514b5827fbbc84aaf5a9ee1c
CarlosMedinaSullca <carlos.medina.sullca@gmail.com>
2025-07-11 13:25:22 -0400

    Connected to database

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
+/* ******************************************
+ * This server.js file is the primary file of the 
+ * application. It is used to control the project.
+ *******************************************/
+/* ***********************
+ * Require Statements
+ *************************/
 const express = require('express');
+const mongodb = require('./data/database');
 const app = express();
+const contacts = require('./routes/contacts')
+
+
+/* ***********************
+ * Routes
+ *************************/
+app.use("/", require("./routes"));
+app.use("/contacts", contacts);
+
+
+
+
+/* ***********************
+ * Local Server Information
+ * Values from .env (environment) file
+ *************************/
 
 const port = process.env.PORT || 3000;
 
-app.use("/", require("./routes"));
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {console.log(`Running on port ${port}`)});
+    }
+})
 
-app.listen(port, () => {console.log(`Running on port ${port}`)})
